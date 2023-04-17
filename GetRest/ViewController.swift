6e936e487fbe758d806429288237f441052677d9
leeeeean <leeeeean91@gmail.com>
2023-04-17 16:35:05 +0900

    base

---
+++ GetRest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetRest
+//
+//  Created by 최리안 on 2023/04/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
